9795db3710a4bc8b6f58b50f4d952511f3317931
kevin <kevin@subskribe.com>
2023-04-05 15:57:54 -0600

    ...

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,8 @@ import { WhisperControl } from '@/src/components/WhisperControl';
 export function createCompletionRequest(messages: ReadonlyArray<ChatCompletionRequestMessage>): CreateChatCompletionRequest {
 
   return {
-    model: 'gpt-3.5-turbo',
+    model: 'gpt-4',
+    // model: 'gpt-3.5-turbo',
     temperature: 0.0,
     max_tokens: 256,
     top_p: 1,
@@ -168,6 +169,80 @@ command: summarize
 url: https://www.msnbc.com/
 ...
 END OUTPUT
+
+# Visualizations: 
+
+You also have access to the following visualizations.
+
+Normally GPT can not do this but we're giving you the ability to do so in the
+user interface by mapping this to a visualization that we show to the user.
+
+DO NOT complain about being unable to show visualizations because you can and 
+I'm giving you the power to do so.
+
+Here is list of visualizations you can run, their description, and parameters.  
+
+When you're given a prompt, determine if it maps to one of the visualizations. 
+
+If it does, return a YAML document which contains the name of the visualization, 
+its data. The visualization is just a YAML document with a visualization
+property that contains the name of the visualization.  Do not include text in a 
+response output.
+
+When the output doesn't match any of the visualizations, interpret the text like a
+normal prompt.
+
+Here is the List of visualizations in YAML format:
+
+---
+pie_chart:
+  description: Generate a pie chart from data output from a prompt
+  data: The data will be a YAML list which has dictionary objects which contain id, label, value, and color parameters.
+...
+
+## Examples
+
+Here are some examples of input and output when creating a visualization:
+
+### Example 1
+
+INPUT: Could you please generate a random pie chart?
+BEGIN OUTPUT
+---
+visualization: pie_chart
+data: 
+  - id: 1
+    label: "San Francisco"
+    value: 1500
+    color: "blue"
+  - id: 2
+    label: "Baltimore"
+    value: 100
+    color: "red"
+...
+END OUTPUT
+
+### Example 2
+
+INPUT: Show me the top two cities and the number of votes for each.
+BEGIN OUTPUT
+---
+visualization: pie_chart
+data: 
+  - id: 1
+    label: "San Francisco"
+    value: 1500
+    color: "blue"
+  - id: 2
+    label: "Baltimore"
+    value: 100
+    color: "red"
+...
+END OUTPUT
+
+NEVER explain the visualization with text. Just output the raw YAML.
+
+The prompt must begin with "---" to indicate it's a YAML document.
 
 `
 
