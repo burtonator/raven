a20fd975c5964ec585f6644a9c5fa0882e0bea27
kevin <kevin@subskribe.com>
2023-04-05 11:02:22 -0600

    back out API keys...

--- pages/api/google_cloudspeech.ts
+++ pages/api/google_cloudspeech.ts
@@ -18,7 +18,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   const response = await fetch(`https://content-texttospeech.googleapis.com/v1/text:synthesize?alt=json&key=${API_KEY}`, {
     headers: {
-      //Authorization: 'Bearer ya29.a0Ael9sCNMbfKPwnAhZzcMGk1HOAPU-Xl1B0dG_GzIlDNvuBqOveomfjo68Q96Xep85WDaQPjQ1tsJ843U_7_nBkpuzzfvCb3-ySBkvrCpYLBmsZfyAfGUUJtnZwpL8o7eIi1Y7WNQEPpJ0SCdDdE76dEVKb3MvK9-YAaCgYKAUQSAQ8SFQF4udJh7X9mj3E3QiZvlb_bgXIoRA0169',
       ContentType: 'text/json'
     },
     method: 'POST',
