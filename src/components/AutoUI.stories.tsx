ddb0cada2767aa9c7595e45f9b40a9d0025a728e
kevin <kevin@subskribe.com>
2023-04-18 19:18:05 -0600

    ....

--- src/components/AutoUI.stories.tsx
+++ src/components/AutoUI.stories.tsx
@@ -7,12 +7,257 @@ export default {
 } as Meta
 
 const yaml = `
-Button: 
-  color: primary
-  children: Click Me  
-  
+Grid:
+    container: true
+    spacing: 2
+    sx:
+        marginTop: 10
+        marginBottom: 10
+    children:
+        - Grid:
+            item: true
+            xs: 12
+            sm: 6
+            md: 4
+            children:
+                - Card:
+                    sx:
+                        height: '100%'
+                        display: 'flex'
+                        flexDirection: 'column'
+                    children:
+                        - CardHeader:
+                            title: Basic
+                            titleTypographyProps:
+                                align: 'center'
+                            subheaderTypographyProps:
+                                align: 'center'
+                            subheader: $9.99/month
+                        - CardContent:
+                            sx:
+                                flexGrow: 1
+                            children:
+                                - Typography:
+                                    variant: body2
+                                    color: 'text.secondary'
+                                    children:
+                                        - "10 users included"
+                                        - br()
+                                        - "2 GB of storage"
+                                        - br()
+                                        - "Email support"
+                                        - br()
+                                        - "Help center access"
+                                - List:
+                                    sx:
+                                        marginTop: 2
+                                        bgcolor: 'background.paper'
+                                    children:
+                                        - ListItem:
+                                            disableGutters: true
+                                            children:
+                                                - ListItemIcon:
+                                                    children:
+                                                        - CheckCircleIcon
+                                                - ListItemText:
+                                                    primary: "Feature 1"
+                                        - ListItem:
+                                            disableGutters: true
+                                            children:
+                                                - ListItemIcon:
+                                                    children:
+                                                        - CheckCircleIcon
+                                                - ListItemText:
+                                                    primary: "Feature 2"
+                        - CardActions:
+                            sx:
+                                justifyContent: 'center'
+                            children:
+                                - Button:
+                                    size: 'large'
+                                    children: Choose Plan
+        - Grid:
+            item: true
+            xs: 12
+            sm: 6
+            md: 4
+            children:
+                - Card:
+                    sx:
+                        height: '100%'
+                        display: 'flex'
+                        flexDirection: 'column'
+                    children:
+                        - CardHeader:
+                            title: Pro
+                            titleTypographyProps:
+                                align: 'center'
+                            subheaderTypographyProps:
+                                align: 'center'
+                            subheader: $19.99/month
+                        - CardContent:
+                            sx:
+                                flexGrow: 1
+                            children:
+                                - Typography:
+                                    variant: body2
+                                    color: 'text.secondary'
+                                    children:
+                                        - "20 users included"
+                                        - br()
+                                        - "10 GB of storage"
+                                        - br()
+                                        - "Priority email support"
+                                        - br()
+                                        - "Help center access"
+                                - List:
+                                    sx:
+                                        marginTop: 2
+                                        bgcolor: 'background.paper'
+                                    children:
+                                        - ListItem:
+                                            disableGutters: true
+                                            children:
+                                                - ListItemIcon:
+                                                    children:
+                                                        - CheckCircleIcon
+                                                - ListItemText:
+                                                    primary: "Feature 1"
+                                        - ListItem:
+                                            disableGutters: true
+                                            children:
+                                                - ListItemIcon:
+                                                    children:
+                                                        - CheckCircleIcon
+                                                - ListItemText:
+                                                    primary: "Feature 2"
+                                        - ListItem:
+                                            disableGutters: true
+                                            children:
+                                                - ListItemIcon:
+                                                    children:
+                                                        - CheckCircleIcon
+                                                - ListItemText:
+                                                    primary: "Feature 3"
+                        - CardActions:
+                            sx:
+                                justifyContent: 'center'
+                            children:
+                                - Button:
+                                    size: 'large'
+                                    children: Choose Plan
+        - Grid:
+            item: true
+            xs: 12
+            sm: 6
+            md: 4
+            children:
+                - Card:
+                    sx:
+                        height: '100%'
+                        display: 'flex'
+                        flexDirection: 'column'
+                    children:
+                        - CardHeader:
+                            title: Enterprise
+                            titleTypographyProps:
+                                align: 'center'
+                            subheaderTypographyProps:
+                                align: 'center'
+                            subheader: $49.99/month
+                        - CardContent:
+                            sx:
+                                flexGrow: 1
+                            children:
+                                - Typography:
+                                    variant: body2
+                                    color: 'text.secondary'
+                                    children:
+                                        - "50 users included"
+                                        - br()
+                                        - "30 GB of storage"
+                                        - br()
+                                        - "Phone and email support"
+                                        - br()
+                                        - "Help center access"
+                                        - br()
+                                        - "Advanced analytics"
+                                - List:
+                                    sx:
+                                        marginTop: 2
+                                        bgcolor: 'background.paper'
+                                    children:
+                                        - ListItem:
+                                            disableGutters: true
+                                            children:
+                                                - ListItemIcon:
+                                                    children:
+                                                        - CheckCircleIcon
+                                                - ListItemText:
+                                                    primary: "Feature 1"
+                                        - ListItem:
+                                            disableGutters: true
+                                            children:
+                                                - ListItemIcon:
+                                                    children:
+                                                        - CheckCircleIcon
+                                                - ListItemText:
+                                                    primary: "Feature 2"
+                                        - ListItem:
+                                            disableGutters: true
+                                            children:
+                                                - ListItemIcon:
+                                                    children:
+                                                        - CheckCircleIcon
+                                                - ListItemText:
+                                                    primary: "Feature 3"
+                                        - ListItem:
+                                            disableGutters: true
+                                            children:
+                                                - ListItemIcon:
+                                                    children:
+                                                        - CheckCircleIcon
+                                                - ListItemText:
+                                                    primary: "Feature 4"
+                        - CardActions:
+                            sx:
+                                justifyContent: 'center'
+                            children:
+                                - Button:
+                                    size: 'large'
+                                    children: Choose Plan
 `
 
+// const yaml = `
+// Grid:
+//     container: true
+//     spacing: 2
+//     children:
+//         - Grid:
+//             item: true
+//             children:
+//                 - Button:
+//                     color: primary
+//                     variant: contained
+//                     children:
+//                         Click Me
+//         - Grid:
+//             item: true
+//             children:
+//                 - Button:
+//                     color: secondary
+//                     variant: contained
+//                     children:
+//                         Don't Click Me
+// `
+
+// const yaml = `
+// Button:
+//   color: secondary
+//   variant: contained
+//   children:
+//     Don't Click Me
+// `
 export const BasicButton = () => {
 
   const props = YAML.parse(yaml)
--- src/components/AutoUI.tsx
+++ src/components/AutoUI.tsx
@@ -1,19 +1,240 @@
 import {ElementRef} from './generated-types/ElementRef'
-import { Button } from '@mui/material';
+import {Fragment} from 'react'
 import { Component } from 'react';
+import {
+  AppBar,
+  Autocomplete,
+  Avatar,
+  Backdrop,
+  Badge,
+  Breadcrumbs,
+  Button,
+  ButtonGroup,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Checkbox,
+  Chip,
+  CircularProgress,
+  ClickAwayListener,
+  Collapse,
+  Container,
+  CssBaseline,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Divider,
+  Drawer,
+  Fab,
+  Fade,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  FormHelperText,
+  FormLabel,
+  Grid,
+  Grow,
+  Hidden,
+  Icon,
+  IconButton,
+  Input,
+  InputAdornment,
+  InputBase,
+  InputLabel,
+  LinearProgress,
+  Link,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemIcon,
+  ListItemSecondaryAction,
+  ListItemText,
+  ListSubheader,
+  Menu,
+  MenuItem,
+  MenuList,
+  MobileStepper,
+  Modal,
+  NativeSelect,
+  NoSsr,
+  OutlinedInput,
+  Paper,
+  Popover,
+  Popper,
+  Portal,
+  Radio,
+  RadioGroup,
+  Rating,
+  ScopedCssBaseline,
+  Select,
+  Slide,
+  Slider,
+  Snackbar,
+  SnackbarContent,
+  Step,
+  StepButton,
+  StepConnector,
+  StepContent,
+  StepIcon,
+  StepLabel,
+  Stepper,
+  SvgIcon,
+  SwipeableDrawer,
+  Switch,
+  Tab,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableFooter,
+  TableHead,
+  TablePagination,
+  TableRow,
+  TableSortLabel,
+  Tabs,
+  TextField,
+  TextareaAutosize,
+  Toolbar,
+  Tooltip,
+  Typography
+} from '@mui/material';
 
 export type ElementToComponentMap = {
   readonly [key in keyof ElementRef]: JSX.Element
 }
 
 const elementToComponentMap: ElementToComponentMap = {
-  Button: Button
+  AppBar: AppBar,
+  Autocomplete: Autocomplete,
+  Avatar: Avatar,
+  Backdrop: Backdrop,
+  Badge: Badge,
+  Breadcrumbs: Breadcrumbs,
+  Button: Button,
+  ButtonGroup: ButtonGroup,
+  Card: Card,
+  CardActionArea: CardActionArea,
+  CardActions: CardActions,
+  CardContent: CardContent,
+  CardHeader: CardHeader,
+  CardMedia: CardMedia,
+  Checkbox: Checkbox,
+  Chip: Chip,
+  CircularProgress: CircularProgress,
+  ClickAwayListener: ClickAwayListener,
+  Collapse: Collapse,
+  Container: Container,
+  CssBaseline: CssBaseline,
+  Dialog: Dialog,
+  DialogActions: DialogActions,
+  DialogContent: DialogContent,
+  DialogContentText: DialogContentText,
+  DialogTitle: DialogTitle,
+  Divider: Divider,
+  Drawer: Drawer,
+  // ExpansionPanel: ExpansionPanel,
+  // ExpansionPanelActions: ExpansionPanelActions,
+  // ExpansionPanelDetails: ExpansionPanelDetails,
+  // ExpansionPanelSummary: ExpansionPanelSummary,
+  Fab: Fab,
+  Fade: Fade,
+  FormControl: FormControl,
+  FormControlLabel: FormControlLabel,
+  FormGroup: FormGroup,
+  FormHelperText: FormHelperText,
+  FormLabel: FormLabel,
+  Grid: Grid,
+  // GridList: GridList,
+  // GridListTile: GridListTile,
+  // GridListTileBar: GridListTileBar,
+  Grow: Grow,
+  Hidden: Hidden,
+  Icon: Icon,
+  IconButton: IconButton,
+  Input: Input,
+  InputAdornment: InputAdornment,
+  InputBase: InputBase,
+  InputLabel: InputLabel,
+  LinearProgress: LinearProgress,
+  Link: Link,
+  List: List,
+  ListItem: ListItem,
+  ListItemAvatar: ListItemAvatar,
+  ListItemIcon: ListItemIcon,
+  ListItemSecondaryAction: ListItemSecondaryAction,
+  ListItemText: ListItemText,
+  ListSubheader: ListSubheader,
+  Menu: Menu,
+  MenuItem: MenuItem,
+  MenuList: MenuList,
+  MobileStepper: MobileStepper,
+  Modal: Modal,
+  // MuiThemeProvider: MuiThemeProvider,
+  NativeSelect: NativeSelect,
+  NoSsr: NoSsr,
+  OutlinedInput: OutlinedInput,
+  Paper: Paper,
+  Popover: Popover,
+  Popper: Popper,
+  Portal: Portal,
+  Radio: Radio,
+  RadioGroup: RadioGroup,
+  Rating: Rating,
+  // RefreshIndicator: RefreshIndicator,
+  // RootRef: RootRef,
+  ScopedCssBaseline: ScopedCssBaseline,
+  Select: Select,
+  Slide: Slide,
+  Slider: Slider,
+  Snackbar: Snackbar,
+  SnackbarContent: SnackbarContent,
+  Step: Step,
+  StepButton: StepButton,
+  StepConnector: StepConnector,
+  StepContent: StepContent,
+  StepIcon: StepIcon,
+  StepLabel: StepLabel,
+  Stepper: Stepper,
+  SvgIcon: SvgIcon,
+  SwipeableDrawer: SwipeableDrawer,
+  Switch: Switch,
+  Tab: Tab,
+  Table: Table,
+  TableBody: TableBody,
+  TableCell: TableCell,
+  TableContainer: TableContainer,
+  TableFooter: TableFooter,
+  TableHead: TableHead,
+  TablePagination: TablePagination,
+  TableRow: TableRow,
+  TableSortLabel: TableSortLabel,
+  Tabs: Tabs,
+  TextField: TextField,
+  TextareaAutosize: TextareaAutosize,
+  Toolbar: Toolbar,
+  Tooltip: Tooltip,
+  Typography: Typography
 }
-
 const NotImplemented = () => {
   return (
     <div>Not implemented</div>
   )
+}
+
+type Primitive = string | number | boolean
+
+function isPrimitive(val: any): val is Primitive {
+  const t = typeof val
+  return t === 'string' || t === 'number' || t === 'boolean'
+}
+
+function isArrayTypeGuard(val: any): val is ReadonlyArray<Primitive | ElementRef> {
+  return Array.isArray(val)
 }
 
 export function AutoUI(props: ElementRef) {
@@ -24,12 +245,25 @@ export function AutoUI(props: ElementRef) {
 
   const Component = elementToComponentMap[elementName] ?? <div>Not implemented: {elementName} </div>
 
-  console.log("FIXME: elementName: ", elementName)
-
   const componentProps = props[elementName]
 
+  const children = componentProps.children
+
+  delete componentProps.children
+
+
   return (
-    <Component {...componentProps}/>
+    <Component {...componentProps}>
+
+      {isPrimitive(children) && <>{children}</>}
+
+      {isArrayTypeGuard(children) && children.map((child, idx) => (
+        <Fragment key={idx}>
+          {isPrimitive(child) && <>{child}</>}
+          {! isPrimitive(child) && <AutoUI {...child}/>}
+        </Fragment>
+      ))}
+    </Component>
   )
 
 }
